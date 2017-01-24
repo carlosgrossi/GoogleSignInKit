7b282e0c3ce4326f1fd5f9b0197eb81cc08bf5ba
Carlos Grossi <carlos.grossi@gmail.com>
2017-01-24 20:39:46 -0200

    Initial Commit

---
+++ GoogleSignInKit/GoogleSignInKit.h
@@ -0,0 +1,19 @@
+//
+//  GoogleSignInKit.h
+//  GoogleSignInKit
+//
+//  Created by Carlos Grossi on 24/01/17.
+//  Copyright © 2017 Carlos Grossi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleSignInKit.
+FOUNDATION_EXPORT double GoogleSignInKitVersionNumber;
+
+//! Project version string for GoogleSignInKit.
+FOUNDATION_EXPORT const unsigned char GoogleSignInKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleSignInKit/PublicHeader.h>
+
+
